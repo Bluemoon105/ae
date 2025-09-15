d96fbcbc3c610fcf26d9271145cbb3140328a1f3
unknown <lyj69641@gmail.com>
2025-09-16 00:35:12 +0900

    ✨ Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './shared/prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
+import { EventModule } from './event/event.module';
+import { MembershipModule } from './membership/membership.module';
+import { NoticeModule } from './notice/notice.module';
+import { NotificationModule } from './notification/notification.module';
+import { ShopModule } from './shop/shop.module';
+import { ContentsModule } from './contents/contents.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule, AuthModule, PostModule, EventModule, MembershipModule, NoticeModule, NotificationModule, ShopModule, ContentsModule, AdminModule] ,
   controllers: [AppController],
   providers: [AppService],
 })
